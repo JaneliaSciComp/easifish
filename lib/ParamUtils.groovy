6c4a86ca23e01d3b059dd72cb5454a44c33b1cb1
Cristian Goina <goinac@janelia.hhmi.org>
2026-02-19 21:49:39 -0500

    fixed file return

--- lib/ParamUtils.groovy
+++ lib/ParamUtils.groovy
@@ -5,10 +5,7 @@ class ParamUtils {
     static def get_params_as_list_of_files(List<String> lparams) {
         lparams
             .findAll { cond -> cond }
-            .collect { f ->
-                Nextflow.log.info "!!!!! F= $f"
-                file(f)
-            }
+            .collect { f -> Nextflow.file(f) }
     }
 
     static def get_warped_subpaths(params) {
